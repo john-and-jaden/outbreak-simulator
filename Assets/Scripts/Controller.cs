66ad11c085d6359476192812660c779f165322f7
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-03-28 13:41:10 -0700

    implement people, movement and controller

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private List<Person> people;
+    public int numPeople;
+    public float distance;
+    public Person personPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        people = new List<Person>();
+        int sideLength = (int)Mathf.Sqrt(numPeople);
+        for (int i = 0; i < sideLength; i++)
+        {
+            for (int j = 0; j < sideLength; j++)
+            {
+                float x = i * distance - sideLength * distance / 2;
+                float y = j * distance - sideLength * distance / 2;
+                Person person = Instantiate(personPrefab, new Vector3(x, y), Quaternion.identity);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Person.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    public float speed;
+    private Vector3 direction;
+    private bool infected;
+    private Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        rigidbody.velocity = direction * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position += direction * speed * Time.deltaTime;
+        // rigidbody.AddForce(direction * speed);
+        if (infected)
+        {
+            spriteRenderer.color = Color.red;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Vector3 normal = collision.GetContact(0).normal;
+        // direction = Vector3.Reflect(direction, normal);
+        infected = true;
+    }
+}
