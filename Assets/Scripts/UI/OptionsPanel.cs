f9167058388c25e3f98746b88170a0cde25e057d
Jaden Balogh <jadenbalogh@gmail.com>
2020-04-13 17:04:18 -0700

    Add options panel slide animation

---
+++ Assets/Scripts/UI/OptionsPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OptionsPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Animator panelAnimator;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("enter");
+        panelAnimator.SetBool("IsOpen", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("exit");
+        panelAnimator.SetBool("IsOpen", false);
+    }
+}
